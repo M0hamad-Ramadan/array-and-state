935a7893336c113073dfdf31ee4dd71ccf9e3151
mohammed <moaboelyazied@gmail.com>
2024-03-17 06:40:54 +0200

    array-and-input-state-2

--- test.js
+++ test.js
@@ -1,36 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  const [newDevice, setNewDevice] = useState("");
-  const [allDevices, setNewDevices] = useState([
-    "iphone",
-    "sumsang",
-    "windows",
-    "mac",
-  ]);
+
+  const [allDevices, setNewDevice] = useState(["iphone", "sumsang", "windows", "mac"]);
 
   const list = allDevices.map((device, i) => {
     return <li key={i}>{device}</li>;
   });
 
-  function inpHandle(e) {
-    setNewDevice(e.target.value);
-  }
-
   function addDevice(e) {
     e.preventDefault();
-    setNewDevices([...allDevices, newDevice]);
+    setNewDevice([...allDevices, e.target[0].value]);
   }
 
   return (
     <div className="App">
       <ul> {list} </ul>
       <form onSubmit={addDevice}>
-        <input
-          placeholder="device name....."
-          value={newDevice}
-          onChange={inpHandle}
-        />
+        <input placeholder="device name....." />
         <button>add</button>
       </form>
     </div>
